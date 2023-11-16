b97ea19737925bd69481d660c6c4a199e1b17f69
njk7740 <143564945+njk7740@users.noreply.github.com>
2023-11-16 11:14:39 +0900

    Merging (#24)
    * map
    
    * map
    
    * map
    
    * searchMap
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * map
    
    * test
    
    * map
    
    * map
    
    ---------
    
    Co-authored-by: njk7740 <njk7740@gmail.com>

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -1,120 +1,81 @@
     var inputAddress = document.querySelector('#searchAddress').value;
-    var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+
+    var mapContainer = document.getElementById('map'),
     mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3
     };
 
-    // 지도를 생성합니다
     var map = new kakao.maps.Map(mapContainer, mapOption);
 
     if (inputAddress == 'aroundMe') {
-    var infowindow2 = new kakao.maps.InfoWindow({zIndex:1});
-        // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
+    var infowindow = new kakao.maps.InfoWindow({zIndex:1});
         if (navigator.geolocation) {
-
-            // GeoLocation을 이용해서 접속 위치를 얻어옵니다
             navigator.geolocation.getCurrentPosition(function(position) {
 
-                var lat = position.coords.latitude, // 위도
-                    lon = position.coords.longitude; // 경도
+                var lat = position.coords.latitude,
+                    lon = position.coords.longitude;
 
-                var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-                    message = '<div style="padding:5px;">여기가 내 위치</div>'; // 인포윈도우에 표시될 내용입니다
+                var locPosition = new kakao.maps.LatLng(lat, lon);
 
-                // 마커와 인포윈도우를 표시합니다
-                displayMarker(locPosition, message);
+                displayMarker_myLocation(locPosition);
 
                 var places = new kakao.maps.services.Places();
 
                 var callback = function(data, status) {
                     for (var i=0; i<data.length; i++) {
-                        displayMarker2(data[i]);
+                        displayMarker_restaurant(data[i]);
                     }
                     console.log(data);
                 };
 
                 places.categorySearch('FD6', callback, {
-                    // Map 객체를 지정하지 않았으므로 좌표객체를 생성하여 넘겨준다.
                     location: new kakao.maps.LatLng(lat, lon)
                 });
 
               });
 
-        } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-
-            var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
-                message = 'geolocation을 사용할수 없어요..'
-
-            displayMarker(locPosition, message);
         }
 
-        function displayMarker2(place) {
-        // 마커를 생성하고 지도에 표시합니다
-        var marker = new kakao.maps.Marker({
-            map: map,
-            position: new kakao.maps.LatLng(place.y, place.x)
-        });
+        function displayMarker_myLocation(locPosition) {
 
-        // 마커에 클릭이벤트를 등록합니다
-        kakao.maps.event.addListener(marker, 'click', function() {
-            // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-            infowindow2.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-            infowindow2.open(map, marker);
-        });
-        }
+            var imageSrc = '/image/myLocation.png',
+                imageSize = new kakao.maps.Size(45, 50),
+                imageOption = {offset: new kakao.maps.Point(21, 47)};
 
-        // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-        function displayMarker(locPosition, message) {
+            var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
 
-            // 마커를 생성합니다
             var marker = new kakao.maps.Marker({
                 map: map,
-                position: locPosition
+                position: locPosition,
+                image: markerImage
             });
 
-            var iwContent = message, // 인포윈도우에 표시할 내용
-                iwRemoveable = true;
+            marker.setMap(map);
 
-            // 인포윈도우를 생성합니다
-            var infowindow = new kakao.maps.InfoWindow({
-                content : iwContent,
-                removable : iwRemoveable
-            });
-
-            // 인포윈도우를 마커위에 표시합니다
-            infowindow.open(map, marker);
-
-            // 지도 중심좌표를 접속위치로 변경합니다
             map.setCenter(locPosition);
         }
     } else {
         var infowindow = new kakao.maps.InfoWindow({zIndex:1});
 
-        // 주소-좌표 변환 객체를 생성합니다
         var geocoder = new kakao.maps.services.Geocoder();
-        // 주소로 좌표를 검색합니다
         geocoder.addressSearch(inputAddress, function(result, status) {
 
-            // 정상적으로 검색이 완료됐으면
              if (status === kakao.maps.services.Status.OK) {
 
                 var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
                 var coords2 = new kakao.maps.LatLng(result[0].y + 0.005, result[0].x + 0.005);
 
-                // 결과값으로 받은 위치를 마커로 표시합니다
                 var marker = new kakao.maps.Marker({
                     map: map,
                     position: coords
                 });
 
-                // 인포윈도우로 장소에 대한 설명을 표시합니다
                 var infowindow = new kakao.maps.InfoWindow({
                     content: '<div style="width:150px;text-align:center;padding:6px 0;">' + inputAddress + '</div>'
                 });
                 infowindow.open(map, marker);
 
-                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
                 map.setCenter(coords);
 
                 var places = new kakao.maps.services.Places();
@@ -122,32 +83,33 @@
                 var callback = function(data, status) {
                     if (status === kakao.maps.services.Status.OK) {
                         for (var i=0; i<data.length; i++) {
-                            displayMarker(data[i]);
+                            displayMarker_restaurant(data[i]);
                         }
+                    console.log(data);
                     }
                 };
 
-                // 공공기관 코드 검색
                 places.categorySearch('FD6', callback, {
-                    // Map 객체를 지정하지 않았으므로 좌표객체를 생성하여 넘겨준다.
                     location: new kakao.maps.LatLng(result[0].y, result[0].x)
                 });
-
-                function displayMarker(place) {
-                    // 마커를 생성하고 지도에 표시합니다
-                    var marker = new kakao.maps.Marker({
-                        map: map,
-                        position: new kakao.maps.LatLng(place.y, place.x)
-                    });
-
-                    // 마커에 클릭이벤트를 등록합니다
-                    kakao.maps.event.addListener(marker, 'click', function() {
-                        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-                        infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-                        infowindow.open(map, marker);
-                    });
-                }
             }
+        });
+    }
+
+    function displayMarker_restaurant(place) {
+        var marker = new kakao.maps.Marker({
+            map: map,
+            position: new kakao.maps.LatLng(place.y, place.x)
+        });
+
+        kakao.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(
+            '<div style="padding:5px;font-size:12px;">' +
+            '<a href="#" style="font-size:15px;font-weight:bold;">' + place.place_name + '</a>' +
+            '<p>' + place.road_address_name + '</p>' +
+            '</div>'
+            );
+            infowindow.open(map, marker);
         });
     }
 

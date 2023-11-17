a9a84ef275cabe7cda3698759e6044907c6affc9
dbstjwjd <143564887+dbstjwjd@users.noreply.github.com>
2023-11-17 14:27:53 +0900

    Dbstjwjd (#37)
    * 시작화면 레이아ã웃
    
    * ìãnp
    
    * npp
    
    * asdf
    
    * ass
    
    * wa
    
    * asdf
    
    * aaa
    
    * ㅊㅅㅎ
    
    * wer
    
    * cgc
    
    * asdf
    
    * aw
    
    * sdfs
    
    * asdf
    
    * asdf
    
    * Asdf
    
    * sdf
    
    * asdf
    
    * asd
    
    * asdf
    
    * sdf
    
    * sdf

---
+++ src/main/java/com/project/team/SMS/SMSController.java
@@ -0,0 +1,54 @@
+package com.project.team.SMS;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpHeaders;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@Controller
+@RequiredArgsConstructor
+public class SMSController {
+
+    private final SMSService smsService;
+
+    public String createRandomNumber() {
+        Random rand = new Random();
+        String randomNum = "";
+        for (int i = 0; i < 4; i++) {
+            String random = Integer.toString(rand.nextInt(10));
+            randomNum += random;
+        }
+
+        return randomNum;
+    }
+
+    @GetMapping("/send")
+    public String send(@RequestParam String phoneNumber, HttpSession session) {
+        String verKey = createRandomNumber();
+        smsService.sendMessage(phoneNumber, verKey);
+
+        // 세션에 verKey 저장
+        session.setAttribute("verKey", verKey);
+
+        return "registerForm";
+    }
+
+
+    @GetMapping("/sendVerKey")
+    public ResponseEntity<Map<String, String>> getVerificationKey(HttpSession session) {
+        // 세션에서 verKey 가져오기
+        String verKey = (String) session.getAttribute("verKey");
+
+        Map<String, String> response = new HashMap<>();
+        response.put("verKey", verKey);
+        return ResponseEntity.ok(response);
+    }
+
+}
---
+++ src/main/java/com/project/team/SMS/SMSService.java
@@ -0,0 +1,34 @@
+package com.project.team.SMS;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Random;
+
+@Service
+public class SMSService {
+
+    private String apiKey = "NCSEIS05KZ5NXZ4E";
+
+    private String apiSecret = "2OKACTN9PVKAEH5BY8JLDDPKCCOHNE6P";
+
+    public void sendMessage(String phoneNumber, String verKey) {
+        Message coolsms = new Message(apiKey, apiSecret);
+        HashMap<String, String> params = new HashMap();
+        params.put("to", phoneNumber);
+        params.put("from", "01024616781");
+        params.put("type", "SMS");
+        params.put("text", "[팀프로젝트] 인증번호는" + "["+verKey+"]" + "입니다.");
+
+        try {
+            JSONObject obj = (JSONObject) coolsms.send(params);
+        } catch (CoolsmsException e) {
+            e.printStackTrace();
+        }
+    }
+
+}

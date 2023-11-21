6805b93bdea8963c3be60431427fb7f152d9cfde
habinnn <107250227+habinnn@users.noreply.github.com>
2023-11-21 11:22:32 +0900

    Team1 (#47)
    * reserve
    
    * reserve
    
    * 123
    
    * asd
    
    * s

--- src/main/java/com/project/team/User/SiteUserController.java
+++ src/main/java/com/project/team/User/SiteUserController.java
@@ -1,8 +1,13 @@
 package com.project.team.User;
 
+import com.project.team.Reservation.Reservation;
+import com.project.team.Reservation.ReservationService;
+import jakarta.validation.Path;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -10,12 +15,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class SiteUserController {
     private final SiteUserService siteUserService;
+    private final ReservationService reservationService;
 
 
     @GetMapping("/signup")
@@ -54,9 +64,17 @@ public class SiteUserController {
     @GetMapping("/userDetail/{loginId}")
     public String userDetail(Model model, @PathVariable("loginId") String loginId) {
         SiteUser siteUser = this.siteUserService.getUser(loginId);
+        List<Reservation> userReservation = this.reservationService.getAllByUser(siteUser);
+        model.addAttribute("userReservation", userReservation);
         model.addAttribute("siteUser", siteUser);
         return "userDetail";
     }
+
+
+
+
+
+
 
 
 
--- src/test/java/com/project/team/TeamApplicationTests.java
+++ src/test/java/com/project/team/TeamApplicationTests.java
@@ -1,18 +1,59 @@
 package com.project.team;
 
+import com.project.team.Restaurant.Restaurant;
+import com.project.team.Restaurant.RestaurantRepository;
+import com.project.team.Restaurant.RestaurantService;
+import com.project.team.User.SiteUser;
+import com.project.team.User.SiteUserRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+<<<<<<< HEAD
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+=======
+>>>>>>> 55fe119d997b7e3e4b43f600020c49a24690a81c
 
 @SpringBootTest
 
 class TeamApplicationTests {
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+	@Autowired
+	private SiteUserRepository siteUserRepository;
+
 
 	@Test
 
 	void contextLoads() {
 
+<<<<<<< HEAD
+
+
+		Restaurant r1 = new Restaurant();
+		r1.setName("떡반집");
+		r1.setAddress("대전 서구 둔산로 8");
+		r1.setNumber("042-472-2921");
+		r1.setOwner(null);
+		r1.setRegDate(LocalDateTime.now());
+		r1.setMain("떡반");
+
+
+
+
+
+
+
+
+
+
+		this.restaurantRepository.save(r1);
+=======
+>>>>>>> 55fe119d997b7e3e4b43f600020c49a24690a81c
 	}
 
 }

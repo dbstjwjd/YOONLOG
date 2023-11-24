5f0f9ed26977fa154a04bdb04c61117ed97e75ab
habinnn <107250227+habinnn@users.noreply.github.com>
2023-11-24 17:45:50 +0900

    Team1 (#68)
    * reserve
    
    * reserve
    
    * 123
    
    * asd
    
    * s
    
    * j
    
    * habin
    
    * habin
    
    * s
    
    * s
    
    * habinnn

--- src/main/java/com/project/team/SecurityConfig.java
+++ src/main/java/com/project/team/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 @Configuration

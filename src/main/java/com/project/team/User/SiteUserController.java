0bb39adef591986c5474c070a7c94086ee914cdf
dbstjwjd <wjddbstj1@naver.com>
2023-11-27 16:44:22 +0900

    ì중간수정

--- src/main/java/com/project/team/User/SiteUserController.java
+++ src/main/java/com/project/team/User/SiteUserController.java
@@ -6,6 +6,7 @@ import com.project.team.Reservation.ReservationService;
 import com.project.team.test.MailDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.dom4j.rule.Mode;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -39,12 +40,14 @@ public class SiteUserController {
     }
 
     @PostMapping("/signup")
-    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
+            model.addAttribute("signupError", "에러 메시지");
             return "start";
         }
         if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
             bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 비밀번호가 일치하지 않습니다.");
+            model.addAttribute("signupError", "에러 메시지");
             return "start";
         }
         try {
@@ -52,18 +55,20 @@ public class SiteUserController {
         } catch (DataIntegrityViolationException e) {
             e.printStackTrace();
             bindingResult.reject("signupFailed", "이미 등록된 사용자 입니다.");
+            model.addAttribute("signupError", "에러 메시지");
             return "start";
         } catch (Exception e) {
             e.printStackTrace();
             bindingResult.reject("signupFailed", e.getMessage());
+            model.addAttribute("signupError", "에러 메시지");
             return "start";
         }
         return "redirect:/";
     }
 
     @GetMapping("/login")
-    public String login() {
-        return "redirect:/";
+    public String login(UserCreateForm userCreateForm) {
+        return "start";
     }
 
     @GetMapping("/userDetail/{loginId}")
